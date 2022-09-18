eb0b231e47c83a6c19790317b04c8f64c397fcf8
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-09-18 14:57:24 +0300

    #1 | Init sanity

---
+++ packages/sanity/schemas/schema.ts
@@ -0,0 +1,7 @@
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import createSchema from "part:@sanity/base/schema-creator";
+
+export default createSchema({
+  name: "default",
+  types: schemaTypes.concat([]),
+});
