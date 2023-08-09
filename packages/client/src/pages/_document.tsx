37265a1836e7536ef57239c2c3c23918cf200814
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-08-09 12:13:12 +0300

    Add google tag manager

--- packages/client/src/pages/_document.tsx
+++ packages/client/src/pages/_document.tsx
@@ -15,6 +15,23 @@ class Document extends NextDocument {
     return (
       <Html lang="en" dir="ltr">
         <Head>
+          <script
+            type="text/javascript"
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w, d, s, l, i) {
+                    w[l] = w[l] || []
+                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+                    const f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s),
+                      dl = l != "dataLayer" ? "&l=" + l : ""
+                    j.async = true
+                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+                    f.parentNode.insertBefore(j, f)
+                  })(window,document,'script','dataLayer',"GTM-NKQP5249")`,
+            }}
+          />
           <meta name="application-name" content={appConfig.APP_NAME} />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
@@ -42,6 +59,14 @@ class Document extends NextDocument {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-NKQP5249`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
