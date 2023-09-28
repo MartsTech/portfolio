90cf474eb6dcff1e7cb7f11e6ffaf3e35986ea91
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-09-28 12:33:42 +0300

    Change google analytics

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { storeWrapper } from "@app/store";
 import "@app/styles/globals.css";
 import type { AppPropsWithLayout } from "@app/types/page";
-import GTM from "@features/gtm/GTM";
 import { Provider as StoreProvider } from "react-redux";
 
 const App = ({ Component, ...rest }: AppPropsWithLayout) => {
@@ -11,18 +10,9 @@ const App = ({ Component, ...rest }: AppPropsWithLayout) => {
 
   return (
     <>
-      <GTM containerId="GTM-NKQP5249" />
       <StoreProvider store={store}>
         {getLayout(<Component {...props.pageProps} />)}
       </StoreProvider>
-      <noscript>
-        <iframe
-          src={"https://www.googletagmanager.com/ns.html?id=GTM-NKQP5249"}
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
     </>
   );
 };
--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { storeWrapper } from "@app/store";
 import type { NextPageWithLayout } from "@app/types/page";
 import HomeModule from "@features/home/HomeModule";
 import { profileApi } from "@features/profile/profile-api";
+import Script from "next/script";
 import type { ReactElement } from "react";
 
 const HomePage: NextPageWithLayout = () => {
@@ -11,7 +12,20 @@ const HomePage: NextPageWithLayout = () => {
 };
 
 HomePage.getLayout = (page: ReactElement) => {
-  return <PageLayout>{page}</PageLayout>;
+  return (
+    <PageLayout>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-QG4MT4EMFJ" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QG4MT4EMFJ');
+        `}
+      </Script>
+      {page}
+    </PageLayout>
+  );
 };
 
 export const getStaticProps = storeWrapper.getStaticProps((store) => {
